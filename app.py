64c17784f2ae2d31ec8e26f6b943f14cdd2fc122
MatVit2910 <mateoviteri13579@gmail.com>
2025-07-13 18:56:06 -0500

    Fixed home.html

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html", name="Mateo")
 
+@app.route('/portfolio')
+def portfolio():
+    return render_template("portfolio.html")
+
 if __name__ == '__main__':
         app.run(debug=True, host="0.0.0.0")
